600cb300885c1d7c881ef824988c96287a05c311
NadetDev <nadetdev@gmail.com>
2025-07-20 19:29:11 +0200

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container py-4">
+    <div class="row">
+      <div class="col-12">
+        <form @submit.prevent="onSubmit">
+          <div class="form-group">
+            <label for="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              class="form-control"
+              v-model="form.firstName"
+            />
+          </div>
+          <div class="form-group">
+            <label for="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              class="form-control"
+              v-model="form.lastName"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              class="form-control"
+              v-model="form.email"
+            />
+          </div>
+          <div class="form-group mt-4">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (!this.formIsValid) return;
+      console.log('form send');
+    },
+  },
+  computed: {
+    formIsValid() {
+      return (
+        this.form.firstName.length > 0 &&
+        this.form.lastName.length > 0 &&
+        this.form.email.length > 0 &&
+        this.form.email.includes('@')
+      );
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
